f5f545fde16397526daff14909a91a87955b0c80
ImLvna <44179559+ImLvna@users.noreply.github.com>
2021-02-15 13:10:26 -0500

    Add files via upload

---
+++ js/invite.js
@@ -0,0 +1,3 @@
+jQuery.get('https://raw.githubusercontent.com/ImLvna/MKXR/main/invite.txt', function(data) {
+    document.location.href = data;
+});
---
+++ monaco.js
@@ -0,0 +1,2 @@
+require.config({ paths: { vs: '../node_modules/monaco-editor/min/vs' } });
+requirejs(['jquery', 'canvas', 'app/sub'],
